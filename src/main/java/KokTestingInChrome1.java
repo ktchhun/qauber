ac24e2d5f1a587fb189fb39a13c7856b6cace178
ktchhun <ktchhun@gmail.com>
2017-03-01 10:15:19 -0800

    Update and rename KokTestingInChrome.java to KokTestingInChrome1.java

--- src/main/java/KokTestingInChrome1.java
+++ src/main/java/KokTestingInChrome1.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class KokTestingInChrome {
+public class KokTestingInChrome1 {
 	
 	public static void main(String[] args) throws Exception
 	{
